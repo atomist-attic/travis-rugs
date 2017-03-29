21ddfce38563802602bf1de17c85e32bda9bde79
Christian Dupuis <cd@atomist.com>
2017-03-29 20:51:12 +0100

    Cleanup

--- .atomist/handlers/event/Build.ts
+++ .atomist/handlers/event/Build.ts
@@ -1,11 +1,13 @@
-import { HandleEvent, Message } from '@atomist/rug/operations/Handlers'
+import { HandleEvent, Message, Plan } from '@atomist/rug/operations/Handlers'
 import { GraphNode, Match, PathExpression } from '@atomist/rug/tree/PathExpression'
 import { EventHandler, Tags } from '@atomist/rug/operations/Decorators'
 
+import { Build } from '@atomist/cortex/Build'
 
 @EventHandler("TravisBuilds", "Handle build events",
-    new PathExpression<GraphNode, GraphNode>(
+    new PathExpression<Build, Build>(
         `/Build
+            [@platform='travis']
             [/hasBuild::Commit()
                 [/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]
                 [/isTagged::Tag()]?]
@@ -15,32 +17,34 @@ import { EventHandler, Tags } from '@atomist/rug/operations/Decorators'
                     [/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]
                 [/on::Repo()]]`))
 @Tags("ci", "travis")
-class Built implements HandleEvent<GraphNode, GraphNode> {
-    handle(event: Match<GraphNode, GraphNode>): Message {
-        let build = event.root() as any
+class Built implements HandleEvent<Build, Build> {
+    handle(event: Match<Build, Build>): Plan {
+        let build = event.root()
+        let plan = new Plan()
 
         let message = new Message()
         message.withNode(build)
 
         let repo = build.on().name()
-
-        let cid = "commit_event/" + build.on().owner() + "/" + repo + "/" + build.hasBuild().sha()
+        let cid = "commit_event/" + build.on().owner() + "/" + repo + "/" + build.ofCommit().sha()
         message.withCorrelationId(cid)
-
 
         // TODO split this into two handlers with proper tree expressions with predicates
         if (build.status() == "Passed" || build.status() == "Fixed") {
+            console.log(JSON.stringify(build.ofCommit().author().of().hasChatIdentity().id()))
             if (build.status() == "Fixed") {
-                if (build.hasBuild().author().hasGithubIdentity() != null) {
-                    message.body = `Travis CI build ${build.name()} of repo ${repo} is now fixed`
-                    message.channelId = build.hasBuild().author().hasGitHubIdentity().hasChatIdentity().id()
+                if (build.ofCommit().author() != null && build.ofCommit().author().of() != null) {
+                    let dmMessage = new Message()
+                    dmMessage.body = `Travis CI build ${build.name()} of repo ${repo} is now fixed`
+                    dmMessage.channelId = build.ofCommit().author().of().hasChatIdentity().id()
+                    plan.add(dmMessage)
                 }
             }
             message.addAction({
                 label: 'Release',
                 instruction: {
                     kind: "command",
-                    name: { group: "atomist-rugs", artifact: "github-handlers", name: "CreateGithubRelease" },
+                    name: { group: "atomist-rugs", artifact: "github-handlers", name: "CreateGitHubRelease" },
                     parameters: {
                         owner: build.on().owner(),
                         repo: build.on().name()
@@ -49,10 +53,12 @@ class Built implements HandleEvent<GraphNode, GraphNode> {
             })
         }
         else if (build.status() == "Failed" || build.status() == "Broken" || build.status() == "Still Failing") {
-            if (build.hasBuild().author().hasGithubIdentity() != null) {
-                let commit = "`" + build.hasBuild().sha() + "`"
-                message.body = `Travis CI build ${build.name()} of repo ${repo} failed after your last commit ${commit}: ${build.build_url()}`
-                message.channelId = build.hasBuild().author().hasGitHubIdentity().hasChatIdentity().id()
+            if (build.ofCommit().author() != null && build.ofCommit().author().of() != null) {
+                let dmMessage = new Message()
+                let commit = "`" + build.ofCommit().sha() + "`"
+                dmMessage.body = `Travis CI build ${build.name()} of repo ${repo} failed after your last commit ${commit}: ${build.buildUrl()}`
+                dmMessage.channelId = build.ofCommit().author().of().hasChatIdentity().id()
+                plan.add(dmMessage)
             }
             message.addAction({
                 label: 'Restart',
@@ -66,15 +72,15 @@ class Built implements HandleEvent<GraphNode, GraphNode> {
                 }
             })
         }
-
-        return message
+        plan.add(message)
+        return plan
     }
 }
 export const built = new Built()
 
 
 @EventHandler("TravisBuildsPrs", "Handle build events from pull-requests", 
-    new PathExpression<GraphNode, GraphNode>(
+    new PathExpression<Build, Build>(
         `/Build
             [/on::Repo()/channel::ChatChannel()]
             [/triggeredBy::PullRequest()
@@ -82,8 +88,8 @@ export const built = new Built()
                 [/contains::Commit()/author::GitHubId()[/hasGithubIdentity::Person()/hasChatIdentity::ChatId()]?]
                 [/on::Repo()]]`))
 @Tags("ci", "travis")
-class PRBuild implements HandleEvent<GraphNode, GraphNode> {
-    handle(event: Match<GraphNode, GraphNode>): Message {
+class PRBuild implements HandleEvent<Build, Build> {
+    handle(event: Match<Build, Build>): Message {
         let build = event.root() as any
 
         let message = new Message()
@@ -98,7 +104,7 @@ class PRBuild implements HandleEvent<GraphNode, GraphNode> {
                 label: 'Release',
                 instruction: {
                     kind: "command",
-                    name: "CreateGithubRelease",
+                    name: { group: "atomist-rugs", artifact: "github-handlers", name: "CreateGitHubRelease" },
                     parameters: {
                         owner: build.on().owner(),
                         repo: build.on().name()
