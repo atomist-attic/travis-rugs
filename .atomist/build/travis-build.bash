563a183d82bea543cb23fb1f2c1cab56f7f4c188
David Dooling <david@atomist.com>
2017-05-03 12:14:33 -0500

    Revert build script, update yarn.lock
    The Travis CI failures were due to a change in the `yarn.lock` file
    format from one patch version of yarn to another.

--- .atomist/build/travis-build.bash
+++ .atomist/build/travis-build.bash
@@ -129,8 +129,6 @@ function main () {
 
     if [[ $TRAVIS_BRANCH == master || $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         msg "publishing archive to $TEAM_ID"
-        git status
-        git diff
         if ! $rug publish -a "$project_version"; then
             err "failed to publish archive $project_version"
             return 1
